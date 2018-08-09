6df16d666d3524cde45648530e7531d17069cacd
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-09 18:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function johnLennonFacts(facts){
 }
 function iLoveTheBeatles(number){
   var list=[]
+  var i=number
   do {
-  var string ="I love the Beatles!" + i
+  var string ="I love the Beatles!"
+  list.push(string);
+  i++
   }
+  while (i<15)
 }
